da876d618e4bc1d94f45ae2fde110426c5f7442b
Hrishikesh Deshpande <92628793+hrishikeshdeshpande111@users.noreply.github.com>
2023-05-27 00:42:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-print("Choose a program to run:")
+header = """   ______      __             ______            __
+  / ____/_  __/ /_  ___  ____/_  __/___  ____  / /
+ / /   / / / / __ \/ _ \/ ___// / / __ \/ __ \/ / 
+/ /___/ /_/ / /_/ /  __/ /   / / / /_/ / /_/ / /  
+\____/\__, /_.___/\___/_/   /_/  \____/\____/_/   
+     /____/                                       """
+
+print(header)
+print('\n')
+print("Welcome to CyberTool! What are you looking for?")
+print('\n')
 print("1. Website Vulnerability Scanner")
 print("2. Image Steganography")
 print("3. Dictionary Attack")
-
-choice = input("Enter your choice (1-3): ")
+print('\n')
+choice = input("Enter your choice: ")
 
 if choice == "1":
     exec(open("webvulnscanner/WebVulnScanner.py").read())
